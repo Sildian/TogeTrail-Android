69139428eb378d0e67120a996867a37c15a4936f
Nicolas MAYER-WEILER <sildian@MacBook-Air-de-Nicolas.local>
2021-12-07 09:17:35 +0100

    [Issue #12] Create base interface and abstract classes for data requests

---
+++ app/src/main/java/com/sildian/apps/togetrail/common/baseDataRequests/BaseDataRequest.kt
@@ -0,0 +1,75 @@
+package com.sildian.apps.togetrail.common.baseDataRequests
+
+import kotlinx.coroutines.*
+
+/*************************************************************************************************
+ * This file provides with base interface and abstract class to run data requests
+ ************************************************************************************************/
+
+/**Base interface for all data requests**/
+
+private interface DataRequest {
+    suspend fun execute()
+    suspend fun cancel()
+}
+
+/**Base for all data requests aiming to load data matching the given type T**/
+
+abstract class LoadDataRequest<T: Any>: DataRequest {
+
+    private var job: Deferred<T?>? = null
+    var data: T? = null
+
+    final override suspend fun execute() {
+        withContext(Dispatchers.IO) {
+            job = async { load() }
+            data = job?.await()
+        }
+    }
+
+    final override suspend fun cancel() {
+        job?.cancel()
+    }
+
+    protected abstract suspend fun load(): T?
+}
+
+/**Base for all data requests aiming to save data matching the given type T**/
+
+abstract class SaveDataRequest<T: Any>(data: T?): DataRequest {
+
+    private var job: Job? = null
+
+    final override suspend fun execute() {
+        withContext(Dispatchers.IO) {
+            job = launch { save() }
+            job?.join()
+        }
+    }
+
+    final override suspend fun cancel() {
+        job?.cancel()
+    }
+
+    protected abstract suspend fun save()
+}
+
+/**Base for all data requests aiming to run specific tasks**/
+
+abstract class SpecificDataRequest: DataRequest {
+
+    private var job: Job? = null
+
+    final override suspend fun execute() {
+        withContext(Dispatchers.IO) {
+            job = launch { run() }
+            job?.join()
+        }
+    }
+
+    final override suspend fun cancel() {
+        job?.cancel()
+    }
+
+    protected abstract suspend fun run()
+}
