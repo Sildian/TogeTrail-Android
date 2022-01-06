18075e07bff9fac68bbac664f28518fc82bb8373
Nicolas MAYER-WEILER <sildian@MacBook-Air-de-Nicolas.local>
2022-01-06 13:16:53 +0100

    [Issue #12] Create event attach / detach trail data requests

---
+++ app/src/main/java/com/sildian/apps/togetrail/event/model/dataRequests/EventAttachTrailDataRequest.kt
@@ -0,0 +1,32 @@
+package com.sildian.apps.togetrail.event.model.dataRequests
+
+import com.sildian.apps.togetrail.common.baseDataRequests.SpecificDataRequest
+import com.sildian.apps.togetrail.event.model.core.Event
+import com.sildian.apps.togetrail.event.model.support.EventRepository
+import com.sildian.apps.togetrail.trail.model.core.Trail
+
+/*************************************************************************************************
+ * Attaches a trail to an event
+ ************************************************************************************************/
+
+class EventAttachTrailDataRequest(
+    private val event: Event?,
+    private val trail: Trail,
+    private val eventRepository: EventRepository
+):
+    SpecificDataRequest()
+{
+
+    override suspend fun run() {
+        this.event?.let { event ->
+            event.id?.let { eventId ->
+                this.trail.id?.let { trailId ->
+                    this.eventRepository.updateEventAttachedTrail(eventId, trail)
+                } ?:
+                throw IllegalArgumentException("Cannot attach a trail without id")
+            } ?:
+            throw IllegalArgumentException("Cannot attach any trail to an event without id")
+        } ?:
+        throw NullPointerException("Cannot attach any trail to a null event")
+    }
+}
