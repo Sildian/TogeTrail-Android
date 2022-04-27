14cb67e7c220b061ad99b18916654fb05b388edd
Nicolas MAYER-WEILER <sildian@MacBook-Air-de-Nicolas.local>
2022-04-27 09:01:35 +0200

    [Issue #15] Create DataHolders to hold data monitored by viewModels

---
+++ app/src/main/java/com/sildian/apps/togetrail/common/baseViewModels/BaseDataHolder.kt
@@ -0,0 +1,19 @@
+package com.sildian.apps.togetrail.common.baseViewModels
+
+import com.sildian.apps.togetrail.common.baseDataRequests.DataRequest
+
+/*************************************************************************************************
+ * This file provides with classes aiming to hold data and expose its state within viewModels
+ ************************************************************************************************/
+
+/**Base for all dataHolders**/
+abstract class DataHolder(val error: Throwable?)
+
+/**Holds a list data**/
+class ListDataHolder<T: Any>(val data: List<T>, error: Throwable? = null): DataHolder(error)
+
+/**Holds a single data**/
+class SingleDataHolder<T: Any>(val data: T?, error: Throwable? = null): DataHolder(error)
+
+/**For data requests which doesn't return any data, this watches the success state**/
+class DataRequestSuccessHolder(val dataRequest: DataRequest, error: Throwable?) : DataHolder(error)
