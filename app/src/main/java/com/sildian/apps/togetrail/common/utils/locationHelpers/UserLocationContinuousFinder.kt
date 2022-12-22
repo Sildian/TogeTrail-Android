b85988ecfad00ef356c1f1cddecc278cd569ab75
Nicolas MAYER-WEILER <n.mayerweiler@gmail.com>
2022-12-22 15:26:24 +0100

    Update Google Play Services

--- app/src/main/java/com/sildian/apps/togetrail/common/utils/locationHelpers/UserLocationContinuousFinder.kt
+++ app/src/main/java/com/sildian/apps/togetrail/common/utils/locationHelpers/UserLocationContinuousFinder.kt
@@ -1,13 +1,16 @@
 package com.sildian.apps.togetrail.common.utils.locationHelpers
 
+import android.content.Context
 import android.os.Looper
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.Priority
 import com.sildian.apps.togetrail.common.utils.DeviceUtilities
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -38,17 +41,19 @@ interface UserLocationContinuousFinder {
 
 @Module
 @InstallIn(SingletonComponent::class)
-object UserLocationContinuousFinderModule {
+interface UserLocationContinuousFinderModule {
 
     @Singleton
-    @Provides
-    fun provideRealUserLocationContinuousFinder(locationProviderClient: FusedLocationProviderClient): UserLocationContinuousFinder =
-        RealUserLocationContinuousFinder(locationProviderClient)
+    @Binds
+    fun bindRealUserLocationContinuousFinder(
+        userLocationContinuousFinder: RealUserLocationContinuousFinder
+    ): UserLocationContinuousFinder
 }
 
 /*********************************Real implementation*******************************************/
 
 class RealUserLocationContinuousFinder @Inject constructor(
+    @ApplicationContext private val context: Context,
     private val locationProviderClient: FusedLocationProviderClient
     )
     : UserLocationContinuousFinder
@@ -56,12 +61,13 @@ class RealUserLocationContinuousFinder @Inject constructor(
 
     @Throws(UserLocationException::class)
     override fun startLocationUpdates(intervalMillis: Long, callback: LocationCallback) {
-        if (DeviceUtilities.isGpsAvailable(locationProviderClient.applicationContext)) {
-            val locationRequest = LocationRequest.create().apply {
-                interval = intervalMillis
-                fastestInterval = intervalMillis
-                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            }
+        if (DeviceUtilities.isGpsAvailable(context)) {
+            val locationRequest = LocationRequest.Builder(
+                Priority.PRIORITY_HIGH_ACCURACY,
+                intervalMillis
+            )
+                .setMinUpdateIntervalMillis(intervalMillis)
+                .build()
             try {
                 this.locationProviderClient.requestLocationUpdates(
                     locationRequest,
--- app/src/main/java/com/sildian/apps/togetrail/common/utils/locationHelpers/UserLocationFinder.kt
+++ app/src/main/java/com/sildian/apps/togetrail/common/utils/locationHelpers/UserLocationFinder.kt
@@ -1,11 +1,13 @@
 package com.sildian.apps.togetrail.common.utils.locationHelpers
 
+import android.content.Context
 import android.location.Location
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.sildian.apps.togetrail.common.utils.DeviceUtilities
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
@@ -32,17 +34,17 @@ interface UserLocationFinder {
 
 @Module
 @InstallIn(SingletonComponent::class)
-object UserLocationFinderModule {
+interface UserLocationFinderModule {
 
     @Singleton
-    @Provides
-    fun provideRealUserLocationFinder(locationProviderClient: FusedLocationProviderClient): UserLocationFinder =
-        RealUserLocationFinder(locationProviderClient)
+    @Binds
+    fun bindRealUserLocationFinder(userLocationFinder: RealUserLocationFinder): UserLocationFinder
 }
 
 /*********************************Real implementation*******************************************/
 
 class RealUserLocationFinder @Inject constructor(
+    @ApplicationContext private val context: Context,
     private val locationProviderClient: FusedLocationProviderClient
     )
     : UserLocationFinder
@@ -60,7 +62,7 @@ class RealUserLocationFinder @Inject constructor(
         when {
             userLocation != null ->
                 return userLocation
-            !DeviceUtilities.isGpsAvailable(locationProviderClient.applicationContext) ->
+            !DeviceUtilities.isGpsAvailable(context) ->
                 throw UserLocationException(UserLocationException.ErrorCode.GPS_UNAVAILABLE)
             else ->
                 throw UserLocationException(UserLocationException.ErrorCode.ERROR_UNKNOWN)
