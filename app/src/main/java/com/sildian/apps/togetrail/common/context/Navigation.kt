a6aea1fb0518751e6d4d28d69b46a89bb1b512f9
Nicolas MAYER-WEILER <n.mayerweiler@gmail.com>
2024-02-18 17:02:12 +0100

    [Issue #22] Set animations to fragment transactions

--- app/src/main/java/com/sildian/apps/togetrail/common/context/Navigation.kt
+++ app/src/main/java/com/sildian/apps/togetrail/common/context/Navigation.kt
@@ -4,11 +4,18 @@ import android.app.Activity
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import com.sildian.apps.togetrail.R
 
 fun FragmentActivity.navigateTo(fragment: Fragment, tag: String, @IdRes container: Int) {
     if (supportFragmentManager.findFragmentByTag(tag) == null) {
         supportFragmentManager
             .beginTransaction()
+            .setCustomAnimations(
+                R.anim.screen_transition_slide_in,
+                R.anim.screen_transition_fade_out,
+                R.anim.screen_transition_fade_in,
+                R.anim.screen_transition_slide_out,
+            )
             .replace(container, fragment, tag)
             .addToBackStack(tag)
             .commit()
